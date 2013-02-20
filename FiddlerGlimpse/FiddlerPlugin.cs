a574cd63345decc5114213d22347537f14a5ee54
Scott Koon <skoon@russell.com>
2013-02-19 16:40:48 -0800

    Initial commit

---
+++ FiddlerGlimpse/FiddlerPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Fiddler;
+
+
+
+namespace FiddlerGlimpse
+{
+    public class FiddlerPlugin : IAutoTamper
+    {
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+            //noop
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+            //noop
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+            //Tell Glimpse
+        }
+
+        public void OnBeforeUnload()
+        {
+            //noop
+        }
+
+        public void OnLoad()
+        {
+            // Tell Glimpse we are ready.
+        }
+
+        private void TellGlimpse()
+        {
+            
+        }
+    }
+}
---
+++ GlimpsePlugin/GlimpsePlugin.cs
@@ -0,0 +1,27 @@
+﻿using Glimpse.Core;
+using Glimpse.Core.Extensibility;
+using System;
+using System.Web;
+
+
+namespace GlimpsePlugin
+{
+
+    public class GlimpsePlugin : IGlimpsePlugin
+    {
+        public object GetData(HttpContextBase context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { return "Fiddler"; }
+        }
+
+        public void SetupInit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
